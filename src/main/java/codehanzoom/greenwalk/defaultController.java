b7ebf8d11300280fcea5be848f28f984169184f9
Youngkwang Lim <richard08@naver.com>
2024-04-05 00:54:48 +0900

    배포 설정 수정 완료 6

---
+++ src/main/java/codehanzoom/greenwalk/defaultController.java
@@ -0,0 +1,13 @@
+package codehanzoom.greenwalk;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class defaultController {
+
+    @GetMapping("/")
+    public String home() {
+        return "<h1>studyHub<h1>";
+    }
+}
